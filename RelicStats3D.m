f291a7be88a71f0ce3e18e54a8b328dbff43af99
Willster419 <willster419@outlook.com>
2016-04-08 20:16:42 -0400

    latest changes

--- RelicStats3D.m
+++ RelicStats3D.m
@@ -1,13 +1,13 @@
 %code for showing the x-y relationship
-x = linspace (1,85,85);
-y = x/(84/9)+ 0.85;
+%x = linspace (1,85,85);
+%y = x/(84/9)+ 0.85;
 %hold all
 %axis ([1 85 1 10]);
 %plot (x,y);
 %hold off
-x2 = linspace(0,1,10);
-y2 = linspace(0,1,10);
-z2 = linspace(0,2,10);
+%x2 = linspace(0,1,10);
+%y2 = linspace(0,1,10);
+%z2 = linspace(0,2,10);
 hold all
-bar3(x2,y2,z2);
+plot3(player,tier,winRate)
 hold off
