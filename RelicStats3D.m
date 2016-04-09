75d3891ca14473ea56e4967b0abd539e8f2a316a
Willster419 <willster419@outlook.com>
2016-04-08 23:13:28 -0400

    latest again lol

--- RelicStats3D.m
+++ RelicStats3D.m
@@ -8,6 +8,13 @@
 %x2 = linspace(0,1,10);
 %y2 = linspace(0,1,10);
 %z2 = linspace(0,2,10);
+%hold all
+ylabel('tier');
+xlabel('player');
+zlabel('WinRate');
+%scatter3(player,winRate,tier)
+%surface(RelicStatsExcelS2)
+%hold off
 hold all
-plot3(player,tier,winRate)
+scatter3(player,tier,winRate);
 hold off
